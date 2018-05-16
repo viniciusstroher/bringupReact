ecc64d4f525bf98cdb2c15e839db808b14069dcf
Vinicius <viniciusferreirawk@gmail.com>
2018-05-16 15:03:17 -0300

    fix

--- scenes/Home.js
+++ scenes/Home.js
@@ -1,5 +1,5 @@
 import React                    from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet,Modal,TouchableHighlight,Button} from "react-native";
 // import Button                   from "react-native-button";
 import {Actions}                from "react-native-router-flux";
 
@@ -23,10 +23,53 @@ var styles = StyleSheet.create({
 });
 
 class Home extends React.Component {
+    state = {
+        modalVisible: false,
+      };
+
+      setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+      }
+
+    onPressAbrepopup(){
+        this.setModalVisible(!this.state.modalVisible);
+    }
     render(){
         return (
             <View style={styles.container}>
                 <Text>Replace screen</Text>
+
+                <Button
+                  onPress={this.onPressAbrepopup.bind(this)}
+                  title="Abre popup" 
+                  color="#841584"
+                  accessibilityLabel="Learn more about this purple button"
+                />
+
+                 <Modal
+                      animationType="slide"
+                      transparent={false}
+                      visible={this.state.modalVisible}
+                      onRequestClose={() => {
+                        // alert('Modal has been closed.');
+                      }}>
+                      <View style={{marginTop: 22}}>
+                        <View>
+                          <Text>Hello World!</Text>
+
+                          <TouchableHighlight
+                            onPress={() => {
+                              this.setModalVisible(!this.state.modalVisible);
+                            }}>
+                            
+                            <Text>Hide Modal</Text>
+                            
+
+
+                          </TouchableHighlight>
+                        </View>
+                      </View>
+                    </Modal>
             </View>
         );
     }
