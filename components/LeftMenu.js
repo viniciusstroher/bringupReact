ff0abb8045a4aa40a91c479b91e02a62da777f61
Vinicius <viniciusferreirawk@gmail.com>
2018-05-16 14:13:18 -0300

    fix

---
+++ components/LeftMenu.js
@@ -0,0 +1,16 @@
+import React                    from 'react';
+import {View, Text, StyleSheet} from "react-native";
+// import Button                   from "react-native-button";
+import {Actions}                from "react-native-router-flux";
+
+class LeftMenu extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>fuckup</Text>
+            </View>
+        );
+    }
+}
+
+module.exports = LeftMenu;
